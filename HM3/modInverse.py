47e1b1e135b836df20f76ac6b25a50a98cf95a25
Matt <mgorgero@uno.edu>
2023-04-16 14:35:16 -0500

    Some stuff done

---
+++ HM3/modInverse.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+def findInverse(num):
+	return 0
+
+def doNothing(minutes):
+
+
+
+def main():
+	#findInverse()
+	digits = doNothing(5)#First 5 minutes,then 60 minutes or 1 hour
+
+if __name__ == "__main__":
+	main()
---
+++ HM3/phi.py
@@ -0,0 +1,39 @@
+from math import gcd
+
+
+
+
+
+
+
+
+
+
+
+
+
+#m = 10, 24, 30
+#https://www.quora.com/How-do-you-check-if-two-numbers-are-coprime-in-Python
+#https://java2blog.com/count-down-for-loop-python/
+def calculateWithOutPhi(m):#lists the number of coprimes that m has that is a number smaller than m
+	f = 0#the count of coprimes between m and i
+	for i in range(m, 0, -1):#counting backwards from m
+		if gcd(m, i) == 1:#if the gcd between the two numbers is 1, then they are coprime
+			f = f + 1
+			print(str(i) + " is coprime with " + str(m))
+	return f
+	
+def calculateWithPhiTotientFunction(m):
+
+def main():
+	print("hello")
+	m = calculateWithOutPhi(10)	
+	print(str(m) + " is the number of coprimes that " + str(10) +  " has without the totient function")
+
+	m = calculateWithPhiTotientFunction(10)
+	print(str(m) + " is the number of coprimes that " + str(10) +  " has with the totient function")
+	
+
+
+if __name__ == "__main__":
+	main()
