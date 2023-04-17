115632aa3855b03cb1c842066355a8e1fefa09d7
Matt <mgorgero@uno.edu>
2023-04-16 19:33:17 -0500

    Did the first part of the second problem where we see how large of a number we can calculate within 5 minutes and 60 minutes

--- HM3/modInverse.py
+++ HM3/modInverse.py
@@ -1,4 +1,4 @@
-
+import time
 
 
 
@@ -11,13 +11,27 @@
 def findInverse(num):
 	return 0
 
+
+#After 5 minutes this is the number: 1932672011
+#After 1 hour, this is the number: 21227158564
 def doNothing(minutes):
+	start = time.time()
+	i = 0
+	while True:
+		potentialEnd = time.time() - start
+		if(potentialEnd >= minutes * 60):# Times 60 to convert it to minutes
+			break
+		i = i + 1
+	
+	return i
+
 
 
 
 def main():
 	#findInverse()
-	digits = doNothing(5)#First 5 minutes,then 60 minutes or 1 hour
+	digits = doNothing(60)#First 5 minutes,then 60 minutes or 1 hour
+	print(digits)
 
 if __name__ == "__main__":
 	main()
